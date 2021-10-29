08273f844fff3056b4f2adf7f5a2662cc4a46f35
devdidar <didarulazammahi@gmail.com>
2021-10-29 19:49:00 +0600

    first commit

---
+++ src/Pages/Home/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            {/* http://pixner.net/emigrar/emigrar/assets/images/all-img/banner01.png */}
+        </div>
+    );
+};
+
+export default Header;
