cd726d5e15da39c7f172b79846832deb484dff9e
devdidar <didarulazammahi@gmail.com>
2021-10-31 22:57:14 +0600

    404 page added

--- src/Pages/Home/NotFound/NotFound.js
+++ src/Pages/Home/NotFound/NotFound.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
     <div>
-      <h1>404</h1>
+      <div className="text-center mt-20 notFond-img">
+        <img
+          src="https://img-16.ccm2.net/_SqzzXVDSG50FWb_UBrCl3XwV78=/440x/1685e17045e747a899925aa16189c7c6/ccm-encyclopedia/99776312_s.jpg"
+          alt=""
+          className="inline-block mb-3"
+        />
+      </div>
+      <div className="text-center">
+        <Link to="/">
+          <button className="bg-indigo-800 text-white px-4 py-2 rounded ">
+            Back to Home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
